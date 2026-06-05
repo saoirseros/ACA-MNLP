45df04b176e3f08804c98c0581c5908054371a0e
Shruti <shruti2904h@gmail.com>
2026-06-05 22:34:12 +0530

    container setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,15 @@ import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element = {<HomePage />}/>
-        <Route path='/login' element = {<LoginPage />}/>
-        <Route path='/profile' element = {<ProfilePage />}/>
-      </Routes>
-    </div>
+    <div className="min-h-screen w-screen bg-black bg-[url('./src/assets/bgImg.svg')] bg-center bg-no-repeat bg-contain"> //bg contain real lifesaver yo
+      <div>
+        <Routes>
+          <Route path='/' element = {<HomePage />}/>
+          <Route path='/login' element = {<LoginPage />}/>
+          <Route path='/profile' element = {<ProfilePage />}/>
+        </Routes>
+      </div>
+    </div> 
   )
 }
 
