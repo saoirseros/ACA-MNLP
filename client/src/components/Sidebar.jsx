5b7e690480b9295ba6d4b82378dac0733a05ee18
Shruti <shruti2904h@gmail.com>
2026-06-10 23:06:22 +0530

    modified search users

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
           <img src={assets.logo} alt="logo" className='max-w-40' />
           
           <div className="relative py-2 group">
-            <img src="assets.menu_icon" alt="menu" className='max-h-5 cursor-pointer' />
+            <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
             
             <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
               <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
@@ -27,7 +27,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
 
         <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
 
-          <img src="{assets.search_icon}" alt="search" className='w-3' />
+          <img src={assets.search_icon} alt="search" className='w-3' />
           <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search user...' />
         </div>
 
