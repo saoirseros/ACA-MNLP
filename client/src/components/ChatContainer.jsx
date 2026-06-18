7c8e9ceb33b1f4abeb4689d1fd95f658c0398886
Shruti <shruti2904h@gmail.com>
2026-06-19 01:28:19 +0530

    designed chat area

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -25,6 +25,24 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
         <img  onClick = {()=> setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
         <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5' />
       </div>
+      
+      {/* chat area */}
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+        {messagesDummyData.map((msg, index)=>(
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+            {msg.image ? (
+              <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
+            ): (
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+            )}
+            <div className='text-center text-xs'>
+              <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
+              <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}></div>
+      </div>
 
     </div>
   ) : (
