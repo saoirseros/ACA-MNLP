97a4e722fa4de00e26615056d5b8c38235e46d84
Shruti <shruti2904h@gmail.com>
2026-06-19 01:34:57 +0530

    profile display

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -5,7 +5,12 @@ const RightSidebar = ({selectedUser}) => {
   return selectedUser && (
     <div>
       <div>
-        <img src={selectedUser?.profilrPic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full'/>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full'/>
+        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}
+          </h1>
+          <p className='px-10 mx-auto'>{selectedUser.bio}</p>
       </div>
     </div>
   )
