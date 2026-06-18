c3d6ae5d2e0f5a3eff73f75b51845d5f2ecf3a3e
Shruti <shruti2904h@gmail.com>
2026-06-19 01:27:30 +0530

    chat header

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import assets, { userDummyData } from '../assets/assets'
+import {messagesDummyData} from '../assets/assets'
+import { useRef } from 'react'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+
+  const scrollEnd = useRef()
+
+  useEffect(()=>{
+    if(scrollEnd.current){
+      scrollEnd.current.scrollIntoView({behavior: "smooth"})
+    }
+  },[])
+
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      {/* header */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
-
         <p className='flex-1 text-lg text-white flex items-center gap-2'>Martin Johnson
           <span className='w-2 h-2 rounded-full bg-green-500'></span>
         </p>
-
         <img  onClick = {()=> setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
         <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5' />
-
       </div>
+
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const RightSidebar = () => {
-  return (
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
     <div>
-      <h1>right sidebar</h1>
+      <div>
+        <img src={selectedUser?.profilrPic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full'/>
+      </div>
     </div>
   )
 }
