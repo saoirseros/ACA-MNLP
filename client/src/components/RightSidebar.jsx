047c1900f437787caa92a85dd805f6821f75c481
Shruti <shruti2904h@gmail.com>
2026-06-19 23:08:39 +0530

    prettier profile info

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -3,8 +3,8 @@ import assets from '../assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
   return selectedUser && (
-    <div>
-      <div>
+    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""}`}>
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full'/>
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
           <p className='w-2 h-2 rounded-full bg-green-500'></p>
