c3f10991fc2189f07e73531b0320e02233e0d365
Shruti <shruti2904h@gmail.com>
2026-06-20 00:05:22 +0530

    added media collection

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { imagesDummyData } from '../assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
   return selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""}`}>
+
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full'/>
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
@@ -11,6 +12,19 @@ const RightSidebar = ({selectedUser}) => {
           {selectedUser.fullName}
           </h1>
           <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+
+      <hr className='border-[#ffffff50] my-4'/>
+
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+          {imagesDummyData.map((url, index)=>(
+            <div key={index} onClick={()=> window.open(url)} className='cursor-pointer rounded'>
+              <img src={url} alt="" className='h-full rounded-md' />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
