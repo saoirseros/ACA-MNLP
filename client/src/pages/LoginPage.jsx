8f78804aa6a9d4bb2a44ce772be9f33544d3c8b2
Shruti <shruti2904h@gmail.com>
2026-06-21 18:35:04 +0530

    click properties for login n sign up

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -56,9 +56,9 @@ const LoginPage = () => {
 
       <div className='flex flex-col gap-2'>
         {currState === "Sign up" ? (
-          <p className='text-sm text-gray-600'>Already have an account? <span className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+          <p className='text-sm text-gray-600'>Already have an account? <span onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
         ) : (
-          <p className='text-sm text-gray-600'>Create an account <span className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
+          <p className='text-sm text-gray-600'>Create an account <span onClick={()=>setCurrState("Sign up")} className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
         )}
       </div>
 
