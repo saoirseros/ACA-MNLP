348d4bf49dc976b81ed83a2d26aa6580199884ce
Shruti <shruti2904h@gmail.com>
2026-06-21 20:07:07 +0530

    completed frontend!

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,7 +19,6 @@ const LoginPage = () => {
       return;
     }
   }
-
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
 
