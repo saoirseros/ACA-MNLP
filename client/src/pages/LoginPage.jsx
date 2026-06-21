078fa9fe59550e7ff3786e0b4325fdcbcc2c696d
Shruti <shruti2904h@gmail.com>
2026-06-21 18:24:55 +0530

    created sign up fields

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,6 +4,12 @@ import assets from '../assets/assets'
 const LoginPage = () => {
 
   const [currState, setCurrState] = useState("Sign up")
+  const [fullName, setFullName] = useState("") //to collect the data inputted on the sign upform in these state variables
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [bio, setBio] = useState("")
+
+  const [isDataSubmitted, setDataSubmitted] = useState(false);
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
@@ -17,6 +23,37 @@ const LoginPage = () => {
         {currState}
         <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
       </h2>
+
+      {currState === "Sign up" && !isDataSubmitted && (
+        <input onChange={(e)=>setFullName(e.target.value)} value={fullName}
+         type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
+      )}
+      
+      {!isDataSubmitted && (
+        <>
+        <input onChange={(e)=>setEmail(e.target.value)} value={email}
+         type="email" placeholder='Email Address' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'/> 
+
+        <input onChange={(e)=>setPassword(e.target.value)} value={password}
+         type="password" placeholder='Password' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
+        </>
+      )}
+
+      {currState === "Sign up" && isDataSubmitted && (
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+           rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='provide a short bio...' required></textarea>
+        )
+      }
+
+      <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+        {currState === "Sign up" ? "Create Account" : "Login Now"}
+      </button>
+
+      <div className='flex items-center gap-2 text-sm text-gray-500'>
+        <input type="checkbox" />
+        <p>Agree to the terms of use & privacy policy.</p>
+      </div>
+
       </form>
     </div>
   )
