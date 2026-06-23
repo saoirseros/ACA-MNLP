db043e238f29877c1bca6c2058bd80305943423f
Shruti <shruti2904h@gmail.com>
2026-06-23 16:47:55 +0530

    user auth check

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -59,3 +59,7 @@ export const login = async (req, res) => {
     }
 }
 
+//controller to check if user is authenticated
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user:req.user});
+}
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -17,7 +17,7 @@ export const protectRoute = async (req, res, next)=>{
         next(); //executes the controller function
 
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
         res.json({success: false, message: error.message });
     }
 }
