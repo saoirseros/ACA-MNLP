8cab6428138b269f9085269aa7823cd7cabbf8e5
Shruti <shruti2904h@gmail.com>
2026-06-23 16:29:30 +0530

    Signup function

---
+++ server/controllers/userController.js
@@ -0,0 +1,37 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+//signup new user
+export const signup = async (req, res)=>{
+    const { fullName, email, password, bio } = req.body;
+
+    try {
+        if (!fullName || !email || !password || !bio){
+            return res.json({success: false, message: "Missing Details"})
+        }
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.json({success: false, message: "Account already exists"})
+        }
+
+        //to store hashed pw in the db and not actual pw
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword, bio
+        });
+
+        //token to authenticate user
+        const token = generateToken(newUser._id)
+
+        res.json({success: true, userData: newUser, token, message: "Account created successfully"})
+
+    } catch (error) {
+
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
