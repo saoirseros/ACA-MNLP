a543fd70abb9e9ee2cb0ff33b0d2ca73dfb3e357
Shruti <shruti2904h@gmail.com>
2026-06-29 22:58:59 +0530

    setting up message routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 //creating express app using http server
 const app = express();
@@ -17,6 +18,7 @@ app.use(cors());
 //Routes setup
 app.use("/api/status", (req,res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 //Connect to mongodb
 await connectDB();
