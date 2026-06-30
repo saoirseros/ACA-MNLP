2babe8ee7c404bff7286f7509470344222995dda
Shruti <shruti2904h@gmail.com>
2026-06-30 22:35:46 +0530

    userSocketMap

--- server/server.js
+++ server/server.js
@@ -16,6 +16,26 @@ export const io = new Server(server, {
     cors: {origin: "*"}
 })
 
+//store online users
+export const userSocketMap = {}; //{userId: socketId}
+
+//socket.io connection handler
+io.on("connection", (scoket)=>{
+    const userId = Socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //emit online users to all connected client
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
+
 //middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
