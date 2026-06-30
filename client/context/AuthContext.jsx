a4436e6fb0443de2354ac7cd3a6ae6154f23197c
Shruti <shruti2904h@gmail.com>
2026-06-30 23:16:57 +0530

    function to handle login and signup

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -33,6 +33,28 @@ export const AuthProvider = ({ children })=>{
         }
     }
 
+    //login fucntion to handle user authentication and socket connection
+    const login = async (state, credentials)=>{
+        try {
+            const { data } = await axios.post(`/api/auth/${state}`, credentials);
+            if(data.success){
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.token;
+                setToken(data.token);
+                localStorage.setItem("token", data.token)
+                toast.success(data.message)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+
+
     //connect socket fucntion to handle socket connection and online users updates
     const connectSocket = (userData)=>{
         if(!userData || socket?.connected) return;
