bf2d846cd491a13d304be56a34cc1c2845429da7
Shruti <shruti2904h@gmail.com>
2026-07-03 01:15:52 +0530

    functionality to create account

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import assets from '../assets/assets';
+import { AuthContext } from '../../context/AuthContext';
 
 const ProfilePage = () => {
+
+  const {authUser, updateProfile} = useContext(AuthContext)
 
   const [selectedImg, setSelectedImg] = useState(null)
   const navigate = useNavigate();
@@ -11,7 +14,19 @@ const ProfilePage = () => {
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    navigate('/')
+    if(!selectedImg){
+      await updateProfile({fullName: name, bio});
+      navigate('/');
+      return;
+    }
+
+    const render = new FileRender();
+    ReadableStream.readAsDataURL(selectedImg);
+    render.onload = async ()=>{
+      const base64Image = render.result;
+      await updateProfile({profilePic: base64Image, fullName: name, bio})
+      navigate('/');
+    }
   }
 
   return (
