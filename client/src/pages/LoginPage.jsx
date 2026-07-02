c36c156137357f750c02f607035580df7912c8a1
Shruti <shruti2904h@gmail.com>
2026-07-03 01:08:35 +0530

    functionality to create acc

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import assets from '../assets/assets'
+import { AuthContext } from '../../context/AuthContext'
 
 const LoginPage = () => {
 
@@ -11,6 +12,8 @@ const LoginPage = () => {
 
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const {login} = useContext(AuthContext)
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
@@ -18,6 +21,8 @@ const LoginPage = () => {
       setIsDataSubmitted(true);
       return;
     }
+
+    login(currState=== "Sign up" ? 'signup' : 'login', {fullName, email, password, bio})
   }
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
