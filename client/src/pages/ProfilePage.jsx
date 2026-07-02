b9bc227c4231bd120d5d3f7fe38476f9060dd88b
Shruti <shruti2904h@gmail.com>
2026-07-03 01:23:27 +0530

    updates on profilepage editing

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -9,8 +9,8 @@ const ProfilePage = () => {
 
   const [selectedImg, setSelectedImg] = useState(null)
   const navigate = useNavigate();
-  const [name, setName] = useState("Martin Johnson")
-  const [bio, setBio] = useState("Hi Everyone, I am using QuickChat")
+  const [name, setName] = useState(authUser.fullName)
+  const [bio, setBio] = useState(authUser.bio)
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
@@ -46,7 +46,7 @@ const ProfilePage = () => {
 
            <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
-        <img className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' src={assets.logo_icon} alt="" />
+        <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`}src={assets.logo_icon} alt="" />
       </div>
 
     </div>
