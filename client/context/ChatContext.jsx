72af3d6efb43073f66cec365f4ec7bf01b6a7830
Shruti <shruti2904h@gmail.com>
2026-07-23 20:39:01 +0530

    add function to unsubscribe

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 import { AuthContext } from "./AuthContext";
@@ -75,6 +75,16 @@ export const ChatProvider = ({ children })=>{
             }
         })
     }
+
+    //function to unsubscribe
+    const unsubscribeFromMessages = ()=>{
+        if(socket) socket.off("newMessage");
+    }
+
+    useEffect(()=>{
+        subscribeToMessages();
+        return ()=> unsubscribeFromMessages();
+    }, [socket, selectedUser])
 
 
     const value = {
