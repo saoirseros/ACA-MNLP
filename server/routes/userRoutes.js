d36c5b4f2f0e8b158359327ec60a9689f40166c1
Shruti <shruti2904h@gmail.com>
2026-08-15 23:10:04 +0530

    lil formatting

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { checkAuth, login, signup, updateProfile } from "../controllers/userController.js";
-import { protectRoute } from '../middleware/auth.js'
+import { protectRoute } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
