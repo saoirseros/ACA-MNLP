3a75d9c3fead78472161af597b5e40477403c399
Shruti <shruti2904h@gmail.com>
2026-08-15 23:08:51 +0530

    lil formatting

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     fullName: {type: String, required: true},
-    password: {type: String, required: true, minLength: 6},
+    password: {type: String, required: true, minlength: 6},
     profilePic: {type: String, default: ""},
     bio: {type: String},
-}, {timestamps: true})
+}, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
 
