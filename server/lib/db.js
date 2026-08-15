b46881e4af40a0fd8ee95da3644542d77cf45c8f
Shruti <shruti2904h@gmail.com>
2026-08-15 23:06:34 +0530

    lil formatting

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-//function to connect to the mongodb database
+// Function to connect to the mongodb database
 export const connectDB = async () =>{
     try {
-
         mongoose.connection.on('connected', ()=> console.log('Database Connected'));
-
-        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+       await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`) 
     } catch (error) {
         console.log(error);
     }
