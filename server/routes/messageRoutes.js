0d6a8b8651205bb3236c3b153cbb5842f459c884
Shruti <shruti2904h@gmail.com>
2026-08-15 23:09:33 +0530

    lil formatting

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersForSiderbar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
-messageRouter.get("/users", protectRoute, getUsersForSiderbar);
+messageRouter.get("/users", protectRoute, getUsersForSidebar);
 messageRouter.get("/:id", protectRoute, getMessages);
 messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
-messageRouter.post("/send/:id", protectRoute, sendMessage);
+messageRouter.post("/send/:id", protectRoute, sendMessage)
 
 export default messageRouter;
