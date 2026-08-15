90d95f910771be4e34fe971f3541992a8576cb63
Shruti <shruti2904h@gmail.com>
2026-08-15 23:07:20 +0530

    update

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,30 +1,21 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
-//to confirm user authentication, middleware to protect routes i.e. user is only routed if authenticated
-
+// Middleware to protect routes
 export const protectRoute = async (req, res, next)=>{
     try {
         const token = req.headers.token;
-
-        if (!token) {
-            return res.json({
-                success:false,
-                message:"Unauthorized"
-            });
-        }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const user = await User.findById(decoded.userId).select("-password");
 
-        if(!user) return res.json({success: false, message: "User not found" });
+        if(!user) return res.json({ success: false, message: "User not found" });
 
         req.user = user;
-        next(); //executes the controller function
-
+        next();
     } catch (error) {
         console.log(error.message);
-        res.json({success: false, message: error.message });
+        res.json({ success: false, message: error.message });
     }
 }
